2c7a48201983d0477005e9bcf56844981620cef6
anassagd432 <anassagd33@gmail.com>
2026-02-24 03:06:24 +0100

    fix(build): fix AGDIConfig import in pi-embedded-subscribe.types.ts

--- src/agents/pi-embedded-subscribe.types.ts
+++ src/agents/pi-embedded-subscribe.types.ts
@@ -1,6 +1,6 @@
 import type { AgentSession } from "@mariozechner/pi-coding-agent";
 import type { ReasoningLevel, VerboseLevel } from "../auto-reply/thinking.js";
-import type { AGDIConfig } from "../config/types.autonomous.js";
+import type { AGDIConfig } from "../config/types.openclaw.js";
 import type { HookRunner } from "../plugins/hooks.js";
 import type { BlockReplyChunking } from "./pi-embedded-block-chunker.js";
 
